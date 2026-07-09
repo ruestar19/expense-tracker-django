7ad9dbd378d36b00df1ebd2e14ca4c13d20dbb87
Rupsa Das <rupsadas573@gmail.com>
2026-07-09 18:09:13 +0530

    Replace category input with dropdown

--- expenses/models.py
+++ expenses/models.py
@@ -1,10 +1,24 @@
 from django.db import models
 
-# Create your models here.
 class Expense(models.Model):
+
+    CATEGORY_CHOICES = [
+        ("Food", "Food"),
+        ("Travel", "Travel"),
+        ("Shopping", "Shopping"),
+        ("Bills", "Bills"),
+        ("Entertainment", "Entertainment"),
+        ("Healthcare", "Healthcare"),
+        ("Education", "Education"),
+        ("Other", "Other"),
+    ]
+
     title= models.CharField(max_length=100)
-    amount= models.IntegerField()
-    category= models.CharField(max_length=100)
+
+    amount= models.DecimalField(max_digits=10, decimal_places=2)
+
+    category= models.CharField(max_length=100, choices=CATEGORY_CHOICES)
+
     date= models.DateField()
 
     def __str__(self):
